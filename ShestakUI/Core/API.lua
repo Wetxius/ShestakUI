2ee4bc4314c8353a831b62d3c2e15d9d007b8dc4
Wetxius <Wetxius@users.noreply.github.com>
2023-04-07 14:55:14 +0300

    [10.1.0] Added skin for new AddonList icons. Reduce scrollbar width.

--- ShestakUI/Core/API.lua
+++ ShestakUI/Core/API.lua
@@ -421,7 +421,7 @@ addAPI(scrollFrame)
 T.SkinFuncs = {}
 T.SkinFuncs["ShestakUI"] = {}
 
-function T.SkinScrollBar(frame, minimal)
+function T.SkinScrollBar(frame)
 	frame:StripTextures()
 
 	local frameName = frame.GetName and frame:GetName()
@@ -430,7 +430,7 @@ function T.SkinScrollBar(frame, minimal)
 	local ThumbTexture = frame.ThumbTexture or frame.thumbTexture or _G[frameName and frameName.."ThumbTexture"]
 	local newThumb = frame.Back and frame:GetThumb()
 
-	if T.newPatch then minimal = true end
+	local minimal = frame:GetWidth() < 10
 
 	if UpButton and DownButton then
 		if not UpButton.icon then
@@ -499,12 +499,20 @@ function T.SkinScrollBar(frame, minimal)
 				hooksecurefunc(newThumb, "Show", function(self)
 					frame:SetAlpha(1)
 				end)
+
+				hooksecurefunc(newThumb, "SetShown", function(self, showThumb)
+					if showThumb then
+						frame:SetAlpha(1)
+					else
+						frame:SetAlpha(0)
+					end
+				end)
 			end
 
 			if minimal then
-				UpButton:SetSize(17, 15)
-				DownButton:SetSize(17, 15)
-				newThumb:SetWidth(17)
+				UpButton:SetSize(14, 14)
+				DownButton:SetSize(14, 14)
+				newThumb:SetWidth(14)
 			end
 		end
 	end
@@ -1085,6 +1093,14 @@ function T.SkinIconBorder(frame, parent)
 			border:SetBackdropBorderColor(unpack(C.media.border_color))
 		end
 	end)
+end
+
+function T.ReplaceIconString(frame, text)
+	if not text then text = frame:GetText() end
+	if not text or text == "" then return end
+
+	local newText, count = gsub(text, "|T([^:]-):[%d+:]+|t", "|T%1:14:14:0:0:64:64:5:59:5:59|t")
+	if count > 0 then frame:SetFormattedText("%s", newText) end
 end
 
 local LoadBlizzardSkin = CreateFrame("Frame")
