7837498c56ca6445fccf7989b0797148815e0da0
alexander.losev@finespace.com <alexander.losev@finespace.com>
2023-04-21 23:57:52 +0300

    Added buffs: -- Fortitude of the Bear -- Feign Death
    Added CDs:
    -- Wailing Arrow
    -- Death Chakram
    -- Survival of the Fittest
    -- Fortitude of the Bear
    -- Volley
    -- Trueshot
    -- Aimed Shot
    -- Kill Shot
    -- Ice Trap(updated ID), name changed to Freezing Trap
    -- Tar Trap
    -- Flare
    -- Salvo
    
    Debuffs:
    -- Serpent Sting was commented. Serpentstalker's Trickery apply it with every Aimed Shot.

--- ShestakUI/Config/Filters/FilgerSpells.lua
+++ ShestakUI/Config/Filters/FilgerSpells.lua
@@ -783,6 +783,10 @@ C["filger_spells"] = {
 			{spellID = 288613, unitID = "player", caster = "player", filter = "BUFF"},
 			-- Survival of the Fittest
 			{spellID = 264735, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Fortitude of the Bear
+			{spellID = 272679, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Feign Death
+			{spellID = 5384, unitID = "player", caster = "player", filter = "BUFF"},
 		},
 		{
 			Name = "P_PROC_ICON",
@@ -831,7 +835,7 @@ C["filger_spells"] = {
 			-- Black Arrow
 			{spellID = 194599, unitID = "target", caster = "player", filter = "DEBUFF"},
 			-- Serpent Sting
-			{spellID = 87935, unitID = "target", caster = "player", filter = "DEBUFF"},
+			--{spellID = 87935, unitID = "target", caster = "player", filter = "DEBUFF"},
 			-- Lacerate
 			{spellID = 185855, unitID = "target", caster = "player", filter = "DEBUFF"},
 			-- A Murder of Crows
@@ -891,7 +895,7 @@ C["filger_spells"] = {
 			{spellID = 147362, filter = "CD"},
 			-- Kill Command
 			{spellID = 34026, filter = "CD"},
-			-- Sidewinders
+			-- Sidewinders                     was deleted?
 			{spellID = 214579, filter = "CD"},
 			-- Explosive Shot
 			{spellID = 212431, filter = "CD"},
@@ -905,7 +909,7 @@ C["filger_spells"] = {
 			{spellID = 781, filter = "CD"},
 			-- Dire Beast
 			{spellID = 120679, filter = "CD"},
-			-- Fervor
+			-- Fervor                     was deleted?
 			{spellID = 82726, filter = "CD"},
 			-- Misdirection
 			{spellID = 34477, filter = "CD"},
@@ -913,8 +917,10 @@ C["filger_spells"] = {
 			{spellID = 5384, filter = "CD"},
 			-- Explosive Trap
 			{spellID = 13813, filter = "CD"},
-			-- Ice Trap
-			{spellID = 13809, filter = "CD"},
+			-- Freezing Trap
+			{spellID = 187650, filter = "CD"},
+			-- Tar Trap
+			{spellID = 187698, filter = "CD"},			
 			-- Binding Shot
 			{spellID = 117526, filter = "CD"},
 			-- Master's Call
@@ -935,6 +941,26 @@ C["filger_spells"] = {
 			{spellID = 186257, filter = "CD"},
 			-- Spirit Mend (Pet)
 			{spellID = 90361, filter = "CD"},
+			-- Wailing Arrow
+			{spellID = 355589, filter = "CD"},
+			-- Death Chakram
+			{spellID = 325037, filter = "CD"},
+			-- Survival of the Fittest
+			{spellID = 264735, filter = "CD"},
+			-- Fortitude of the Bear
+			{spellID = 272679, filter = "CD"},
+			-- Volley
+			{spellID = 194386, filter = "CD"},									
+			-- Trueshot
+			{spellID = 288613, filter = "CD"},
+			-- Aimed Shot
+			{spellID = 19434, filter = "CD"},
+			-- Kill Shot
+			{spellID = 53351, filter = "CD"},
+			-- Flare
+			{spellID = 1543, filter = "CD"},
+			-- Salvo
+			{spellID = 400456, filter = "CD"},			
 		},
 	},
 	["MAGE"] = {
