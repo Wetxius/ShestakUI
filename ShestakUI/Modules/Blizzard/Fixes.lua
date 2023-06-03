be3e7064707ab1d66ee97e88d507a1ad45fccfe3
Wetxius <wetxius@gmail.com>
2023-06-03 19:02:57 +0300

    Revert garbage cleaning.

--- ShestakUI/Modules/Blizzard/Fixes.lua
+++ ShestakUI/Modules/Blizzard/Fixes.lua
@@ -49,26 +49,8 @@ end
 _G.SettingsPanel.TransitionBackOpeningPanel = _G.HideUIPanel
 
 ----------------------------------------------------------------------------------------
---	Collect garbage
+-- !!NoTaint2 (Code by warbaby 2022-11 http://abyui.top https://github.com/aby-ui)
 ----------------------------------------------------------------------------------------
-local eventcount = 0
-local Garbage = CreateFrame("Frame")
-Garbage:RegisterAllEvents()
-Garbage:SetScript("OnEvent", function(self, event)
-	eventcount = eventcount + 1
-
-	if not InCombatLockdown() then
-		if eventcount > 10000 or event == "PLAYER_ENTERING_WORLD" then
-			collectgarbage("collect")
-			eventcount = 0
-		end
-	end
-end)
-
-------------------------------------------------------------------------
--- !!NoTaint2, first-aid addon for Dragon Flight action bars taint.
--- Code by warbaby 2022-11 http://abyui.top https://github.com/aby-ui
--------------------------------------------------------------------------
 if IsAddOnLoaded("!!NoTaint2") then return end
 if not NoTaint2_Proc_ResetActionButtonAction then
     NoTaint2_Proc_ResetActionButtonAction = 1
