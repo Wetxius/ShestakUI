33122aa5c72e14755c294a39a109537ed4b4a892
Wetxius <wetxius@gmail.com>
2023-07-13 09:16:08 +0300

    [PulseCD] Allow to add ignore spell in GUI and added new option to change ignore list to whitelist.

--- ShestakUI/Modules/Cooldowns/PulseCD.lua
+++ ShestakUI/Modules/Cooldowns/PulseCD.lua
@@ -112,7 +112,7 @@ local function OnUpdate(_, update)
                     end)
 				end
 				local cooldown = getCooldownDetails()
-				if T.pulse_ignored_spells[cooldown.name] then
+				if (C.pulsecooldown.whitelist and not T.pulse_ignored_spells[cooldown.name]) or (not C.pulsecooldown.whitelist and T.pulse_ignored_spells[cooldown.name]) then
 					watching[i] = nil
 				else
 					if cooldown.enabled ~= 0 then
@@ -129,7 +129,7 @@ local function OnUpdate(_, update)
 		for i, getCooldownDetails in pairs(cooldowns) do
             local cooldown = getCooldownDetails()
             local remaining = cooldown.duration - (GetTime() - cooldown.start)
-            if remaining <= 0 then
+            if remaining <= 0.2 then
                 tinsert(animating, {cooldown.texture, cooldown.isPet, cooldown.name})
                 cooldowns[i] = nil
             end
