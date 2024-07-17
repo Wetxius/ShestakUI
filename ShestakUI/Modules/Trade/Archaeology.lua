c8daa8ddc3df0db924d260b4d6bfa54ee660ab26
Frode <frode.h@aktivportal.com>
2024-07-17 23:58:55 +0200

    Update Archaeology.lua
    Use Middle instead of center

--- ShestakUI/Modules/Trade/Archaeology.lua
+++ ShestakUI/Modules/Trade/Archaeology.lua
@@ -23,7 +23,7 @@ function stArch:OnLoad(self)
 	stArch["title"]["text"] = stArch["title"]:CreateFontString()
 	stArch["title"]["text"]:SetPoint("CENTER", stArch["title"], "CENTER", 0, 0)
 	stArch["title"]["text"]:SetJustifyH("CENTER")
-	stArch["title"]["text"]:SetJustifyV("CENTER")
+	stArch["title"]["text"]:SetJustifyV("MIDDLE")
 	stArch["title"]["text"]:SetFont(C.media.pixel_font, C.media.pixel_font_size, C.media.pixel_font_style)
 	stArch["title"]["text"]:SetText(PROFESSIONS_ARCHAEOLOGY)
 
