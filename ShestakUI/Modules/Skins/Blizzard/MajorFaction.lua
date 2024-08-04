a5befb00ff7fe3152f66ddc43a2404c9def2d0cf
Wetxius <wetxius@gmail.com>
2024-08-04 12:36:24 +0300

    [Skins] Better skin for MajorFaction.

--- ShestakUI/Modules/Skins/Blizzard/MajorFaction.lua
+++ ShestakUI/Modules/Skins/Blizzard/MajorFaction.lua
@@ -8,7 +8,23 @@ local function LoadSkin()
 	local frame = _G.MajorFactionRenownFrame
 
 	T.SkinFrame(frame)
+	MajorFactionRenownFrame.CloseButton.SetPoint = T.dummy
 	frame.Background:SetAlpha(0)
+
+	if frame.LevelSkipButton then
+		frame.LevelSkipButton:SkinButton()
+	end
+
+	hooksecurefunc(frame, "SetUpMajorFactionData", function()
+		if frame.Divider then frame.Divider:Hide() end
+		if frame.NineSlice then frame.NineSlice:Hide() end
+		if frame.Border then frame.Border:Hide() end
+		if frame.TopLeftBorderDecoration then frame.TopLeftBorderDecoration:Hide() end
+		if frame.TopRightBorderDecoration then frame.TopRightBorderDecoration:Hide() end
+		if frame.Background then frame.Background:Hide() end
+		if frame.BackgroundShadow then frame.BackgroundShadow:Hide() end
+		if frame.CloseButton.Border then frame.CloseButton.Border:Hide() end
+	end)
 end
 
 T.SkinFuncs["Blizzard_MajorFactions"] = LoadSkin
