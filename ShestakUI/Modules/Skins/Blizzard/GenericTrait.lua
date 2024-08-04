804ed6d580b5efd1d29ff19e2eb6f8c5b81bedd8
Wetxius <wetxius@gmail.com>
2024-08-04 12:36:35 +0300

    Cleanup.

--- ShestakUI/Modules/Skins/Blizzard/GenericTrait.lua
+++ ShestakUI/Modules/Skins/Blizzard/GenericTrait.lua
@@ -8,16 +8,11 @@ local function LoadSkin()
 	local frame = _G.GenericTraitFrame
 	T.SkinFrame(frame)
 
-	local function ReplaceIconString(frame, text)
-		if not text then text = frame:GetText() end
-		if not text or text == "" then return end
+	frame.Background:SetAlpha(0)
+	frame.BorderOverlay:SetAlpha(0)
 
-		local newText, count = gsub(text, "|T(%d+):24:24[^|]*|t", " |T%1:16:16:0:0:64:64:5:59:5:59|t")
-		if count > 0 then frame:SetFormattedText("%s", newText) end
-	end
-
-	ReplaceIconString(frame.Currency.UnspentPointsCount)
-	hooksecurefunc(frame.Currency.UnspentPointsCount, "SetText", ReplaceIconString)
+	T.ReplaceIconString(frame.Currency.UnspentPointsCount)
+	hooksecurefunc(frame.Currency.UnspentPointsCount, "SetText", T.ReplaceIconString)
 end
 
 T.SkinFuncs["Blizzard_GenericTraitUI"] = LoadSkin
