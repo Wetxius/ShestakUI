ea3dce4aee479361a499c7d135447ce1db274f35
Wetxius <wetxius@gmail.com>
2024-08-10 14:41:46 +0300

    [ActionBars] Don't change color for hotkey.

--- ShestakUI/Modules/ActionBars/Style.lua
+++ ShestakUI/Modules/ActionBars/Style.lua
@@ -98,6 +98,11 @@ local function StyleNormalButton(button, size)
 			hotkey:SetShadowOffset(C.font.action_bars_font_shadow and 1 or 0, C.font.action_bars_font_shadow and -1 or 0)
 			hotkey:SetWidth(C.actionbar.button_size - 1)
 			hotkey:SetHeight(C.font.action_bars_font_size)
+			hooksecurefunc(hotkey, "SetVertexColor", function(_, _, g)
+				if g < 0.8 then
+					hotkey:SetVertexColor(ACTIONBAR_HOTKEY_FONT_COLOR:GetRGB())
+				end
+			end)
 		else
 			hotkey:Kill()
 		end
