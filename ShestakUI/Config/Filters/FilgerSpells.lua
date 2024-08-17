0e1279194d0bee106b3f9d231a3c10b0e3a6e5da
Saltuar <rewse3@gmail.com>
2024-08-17 09:27:32 +0200

    new spellID for find weakness

--- ShestakUI/Config/Filters/FilgerSpells.lua
+++ ShestakUI/Config/Filters/FilgerSpells.lua
@@ -1803,7 +1803,7 @@ C["filger_spells"] = {
 			-- Hemorrhage
 			{spellID = 16511, unitID = "target", caster = "player", filter = "DEBUFF"},
 			-- Find Weakness
-			{spellID = 91021, unitID = "target", caster = "player", filter = "DEBUFF"},
+			{spellID = 316220, unitID = "target", caster = "player", filter = "DEBUFF"},
 			-- Vendetta
 			{spellID = 79140, unitID = "target", caster = "player", filter = "DEBUFF"},
 			-- Nightblade
