8b87ed884d320365ab377cdaf57a5030ddf848d5
Wetxius <wetxius@gmail.com>
2024-08-17 19:21:00 +0300

    [Skins] Fixed AzeriteEssence.

--- ShestakUI/Modules/Skins/Blizzard/AzeriteEssence.lua
+++ ShestakUI/Modules/Skins/Blizzard/AzeriteEssence.lua
@@ -5,38 +5,42 @@ if C.skins.blizzard_frames ~= true then return end
 --	AzeriteEssenceUI skin
 ----------------------------------------------------------------------------------------
 local function LoadSkin()
-	AzeriteEssenceUI:StripTextures()
-	AzeriteEssenceUI:SetTemplate("Transparent")
-
-	AzeriteEssenceUIPortrait:SetAlpha(0)
+	T.SkinFrame(AzeriteEssenceUI)
+	T.SkinScrollBar(AzeriteEssenceUI.EssenceList.ScrollBar)
 
 	AzeriteEssenceUI.PowerLevelBadgeFrame:ClearAllPoints()
 	AzeriteEssenceUI.PowerLevelBadgeFrame:SetPoint("TOPLEFT")
 	AzeriteEssenceUI.PowerLevelBadgeFrame.Ring:Hide()
 	AzeriteEssenceUI.PowerLevelBadgeFrame.BackgroundBlack:Hide()
 
-	for _, button in pairs(AzeriteEssenceUI.EssenceList.buttons) do
-		button:DisableDrawLayer("ARTWORK")
-		button:StyleButton(nil, 3)
+	hooksecurefunc(AzeriteEssenceUI.EssenceList.ScrollBox, "Update", function(self)
+		self:ForEachFrame(function(button)
+			if not button.bg then
+				button:StyleButton(nil, 3)
 
-		button.Icon:SkinIcon()
-		button.Icon:SetSize(27, 27)
-		button.Icon:SetPoint("LEFT", button, "LEFT", 6, 0)
+				button.bg = CreateFrame("Frame", nil, button)
+				button.bg:CreateBackdrop("Overlay")
+				button.bg:SetFrameLevel(button:GetFrameLevel() - 1)
+				button.bg:SetPoint("TOPLEFT", 3, -3)
+				button.bg:SetPoint("BOTTOMRIGHT", -3, 3)
 
-		button.bg = CreateFrame("Frame", nil, button)
-		button.bg:CreateBackdrop("Overlay")
-		button.bg:SetFrameLevel(button:GetFrameLevel() - 1)
-		button.bg:SetPoint("TOPLEFT", 3, -3)
-		button.bg:SetPoint("BOTTOMRIGHT", -3, 3)
-	end
+				if button.Icon then
+					button:DisableDrawLayer("ARTWORK")
+					button.Icon:SkinIcon()
+					button.Icon:SetSize(27, 27)
+					button.Icon:SetPoint("LEFT", button, "LEFT", 6, 0)
+				else
+					button:DisableDrawLayer("BACKGROUND")
+					button:DisableDrawLayer("BORDER")
+				end
 
-	local HeaderButton = AzeriteEssenceUI.EssenceList.HeaderButton
-	HeaderButton.Middle:Hide()
-	HeaderButton.Left:Hide()
-	HeaderButton.Right:Hide()
-
-	T.SkinScrollBar(AzeriteEssenceUI.EssenceList.ScrollBar)
-	T.SkinCloseButton(AzeriteEssenceUICloseButton)
+				if button.PendingGlow then
+					button.PendingGlow:SetColorTexture(0.9, 0.8, 0.1, 0.3)
+					button.PendingGlow:SetInside(button.bg.backdrop)
+				end
+			end
+		end)
+	end)
 end
 
 T.SkinFuncs["Blizzard_AzeriteEssenceUI"] = LoadSkin
