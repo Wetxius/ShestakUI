ba61124fa891de473aa188f09c3a8fb66c419b22
Wetxius <wetxius@gmail.com>
2024-09-29 09:49:10 +0300

    Always reskin new Blizzard menu.

--- ShestakUI/Modules/Skins/Blizzard/Others.lua
+++ ShestakUI/Modules/Skins/Blizzard/Others.lua
@@ -250,6 +250,38 @@ SkinBlizzUI:SetScript("OnEvent", function(_, _, addon)
 			MyFrameDropDownBackdrop:SetTemplate("Transparent")
 		end
 
+		--	Blizzard_Menu skin
+		do
+			local backdrops = {}
+			local function SkinFrame(frame)
+				frame:StripTextures()
+
+				if backdrops[frame] then
+					frame.backdrop = backdrops[frame] -- relink it back
+				else
+					frame:CreateBackdrop("Transparent") -- :SetTemplate errors out
+					frame.backdrop:SetInside(frame, 0, 0)
+					backdrops[frame] = frame.backdrop
+
+					if frame.ScrollBar then
+						T.SkinScrollBar(frame.ScrollBar)
+					end
+				end
+			end
+
+			local function OpenMenu(manager, _, menuDescription)
+				local menu = manager:GetOpenMenu()
+				if menu then
+					SkinFrame(menu)
+					menuDescription:AddMenuAcquiredCallback(SkinFrame)
+				end
+			end
+
+			local manager = _G.Menu.GetManager()
+			hooksecurefunc(manager, "OpenMenu", OpenMenu)
+			hooksecurefunc(manager, "OpenContextMenu", OpenMenu)
+		end
+
 		-- Hide header textures and move text/buttons
 		local BlizzardHeader = {
 			GameMenuFrame,
