2d252eefd573086d49296fc7c62ecea44bc3037e
Wetxius <wetxius@gmail.com>
2025-02-27 12:40:12 +0300

    Cleanup.

--- ShestakUI/Modules/Skins/Blizzard/AddonList.lua
+++ ShestakUI/Modules/Skins/Blizzard/AddonList.lua
@@ -5,32 +5,21 @@ if C.skins.blizzard_frames ~= true then return end
 --	AddonList skin
 ----------------------------------------------------------------------------------------
 local function LoadSkin()
-	if T.newPatch then
-		local buttons = {
-			AddonList.EnableAllButton,
-			AddonList.DisableAllButton,
-			AddonList.CancelButton,
-			AddonList.OkayButton
-		}
+	local buttons = {
+		AddonList.EnableAllButton,
+		AddonList.DisableAllButton,
+		AddonList.CancelButton,
+		AddonList.OkayButton
+	}
 
-		for i = 1, #buttons do
-			local frame = buttons[i]
-			if frame then
-				frame:SkinButton()
-			end
-		end
-	else
-		local buttons = {
-			"AddonListEnableAllButton",
-			"AddonListDisableAllButton",
-			"AddonListCancelButton",
-			"AddonListOkayButton"
-		}
-
-		for _, button in pairs(buttons) do
-			_G[button]:SkinButton()
+	for i = 1, #buttons do
+		local frame = buttons[i]
+		if frame then
+			frame:SkinButton()
 		end
 	end
+
+	AddonList.OkayButton:SetWidth(90)
 
 	AddonList:StripTextures()
 	AddonList:SetTemplate("Transparent")
@@ -46,46 +35,25 @@ local function LoadSkin()
 		self:SetDesaturated(true, true)
 	end
 
-	if T.newPatch then
-		hooksecurefunc("AddonList_InitAddon", function(child)
-			if not child.styled then
-				T.SkinCheckBox(child.Enabled)
-				child.LoadAddonButton:SkinButton()
-				hooksecurefunc(child.Enabled:GetCheckedTexture(), "SetDesaturated", forceSaturation)
+	hooksecurefunc("AddonList_InitAddon", function(child)
+		if not child.styled then
+			T.SkinCheckBox(child.Enabled)
+			child.LoadAddonButton:SkinButton()
+			hooksecurefunc(child.Enabled:GetCheckedTexture(), "SetDesaturated", forceSaturation)
 
-				T.ReplaceIconString(child.Title)
-				hooksecurefunc(child.Title, "SetText", T.ReplaceIconString)
+			T.ReplaceIconString(child.Title)
+			hooksecurefunc(child.Title, "SetText", T.ReplaceIconString)
 
-				child.styled = true
-			end
-		end)
-	else
-		hooksecurefunc("AddonList_InitButton", function(child)
-			if not child.styled then
-				T.SkinCheckBox(child.Enabled)
-				child.LoadAddonButton:SkinButton()
-				hooksecurefunc(child.Enabled:GetCheckedTexture(), "SetDesaturated", forceSaturation)
-
-				T.ReplaceIconString(child.Title)
-				hooksecurefunc(child.Title, "SetText", T.ReplaceIconString)
-
-				child.styled = true
-			end
-		end)
-	end
+			child.styled = true
+		end
+	end)
 
 	T.SkinScrollBar(AddonList.ScrollBar)
 	T.SkinCloseButton(AddonListCloseButton)
 	T.SkinDropDownBox(AddonList.Dropdown)
-	if T.newPatch then
-		T.SkinCheckBox(AddonList.ForceLoad)
-		AddonList.ForceLoad:SetSize(25, 25)
-
-		T.SkinEditBox(AddonList.SearchBox)
-	else
-		T.SkinCheckBox(AddonListForceLoad)
-		AddonListForceLoad:SetSize(25, 25)
-	end
+	T.SkinCheckBox(AddonList.ForceLoad)
+	AddonList.ForceLoad:SetSize(25, 25)
+	T.SkinEditBox(AddonList.SearchBox)
 end
 
 tinsert(T.SkinFuncs["ShestakUI"], LoadSkin)
