11a2e5eee4daea0aa4db380178d333c11579fc61
Wetxius <wetxius@gmail.com>
2026-02-01 15:28:53 +0300

    Prevent secret error in TabBinder.

--- ShestakUI/Modules/Automation/TabBinder.lua
+++ ShestakUI/Modules/Automation/TabBinder.lua
@@ -17,7 +17,7 @@ local RTB_Fail, RTB_DefaultKey, LastTargetKey, TargetKey, CurrentBind, Success =
 TabBinder:SetScript("OnEvent", function(_, event, ...)
 	if event == "CHAT_MSG_SYSTEM" then
 		local RTBChatMessage = ...
-		if RTBChatMessage == ERR_DUEL_REQUESTED then
+		if canaccessvalue(RTBChatMessage) and RTBChatMessage == ERR_DUEL_REQUESTED then
 			event = "DUEL_REQUESTED"
 		end
 	elseif event == "ZONE_CHANGED_NEW_AREA" or event == "PLAYER_ENTERING_WORLD" or (event == "PLAYER_REGEN_ENABLED" and RTB_Fail) or event == "DUEL_REQUESTED" or event == "DUEL_FINISHED" then
