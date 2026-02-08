3c9c8466197f4c3890628406b0d1d752ef62b60e
Wetxius <wetxius@gmail.com>
2026-02-08 20:33:15 +0300

    [Skins] Updated ItemInteraction.

--- ShestakUI/Modules/Skins/Blizzard/ItemInteraction.lua
+++ ShestakUI/Modules/Skins/Blizzard/ItemInteraction.lua
@@ -5,11 +5,9 @@ if C.skins.blizzard_frames ~= true then return end
 --	ItemInteractionUI skin
 ----------------------------------------------------------------------------------------
 local function LoadSkin()
-	ItemInteractionFrame:StripTextures()
-	ItemInteractionFrame:CreateBackdrop("Transparent")
+	T.SkinFrame(ItemInteractionFrame, true)
 
-	ItemInteractionFramePortrait:Hide()
-
+	ItemInteractionFrame.Background:SetAlpha(0)
 	ItemInteractionFrame.ButtonFrame:StripTextures()
 	ItemInteractionFrame.ButtonFrame.MoneyFrameEdge:StripTextures()
 
@@ -25,12 +23,30 @@ local function LoadSkin()
 	ItemSlot.Icon:SetPoint("BOTTOMRIGHT", -1, 1)
 	ItemSlot.Icon:SkinIcon()
 
+	for _, frame in pairs({ItemInteractionFrame.ItemConversionFrame.ItemConversionInputSlot, ItemInteractionFrame.ItemConversionFrame.ItemConversionOutputSlot}) do
+		frame.ButtonFrame:SetAlpha(0)
+		frame.icon:SkinIcon()
+		frame.NormalTexture:SetInside(frame.backdrop)
+		if frame.SelectedTexture then
+			frame.SelectedTexture:SetColorTexture(1, 0.82, 0, 0.2)
+			frame.SelectedTexture:SetInside(frame.backdrop)
+		end
+		if frame.HighlightTexture then
+			frame.HighlightTexture:SetColorTexture(1, 1, 1, 0.2)
+			frame.HighlightTexture:SetInside(frame.backdrop)
+		end
+		if frame.PushedTexture then
+			frame.PushedTexture:SetColorTexture(0.9, 0.8, 0.1, 0.3)
+			frame.PushedTexture:SetInside(frame.backdrop)
+		end
+	end
+
 	ItemSlot.GlowOverlay:SetAlpha(0)
 
+	ItemInteractionFrame.CurrencyCost.Currency.Icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
 	ItemInteractionFrame.ButtonFrame.Currency.Icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
 
 	ItemInteractionFrame.ButtonFrame.ActionButton:SkinButton()
-	T.SkinCloseButton(ItemInteractionFrameCloseButton)
 end
 
 T.SkinFuncs["Blizzard_ItemInteractionUI"] = LoadSkin
