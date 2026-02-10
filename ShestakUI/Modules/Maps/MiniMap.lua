de467fe687fb5bcba61846cf65b562c3100e9703
Wetxius <wetxius@gmail.com>
2026-02-10 17:19:25 +0300

    Cleanup.

--- ShestakUI/Modules/Maps/MiniMap.lua
+++ ShestakUI/Modules/Maps/MiniMap.lua
@@ -499,3 +499,58 @@ if C.minimap.on_top then
 		end
 	end)
 end
+
+----------------------------------------------------------------------------------------
+--	Farm mode for minimap(by Elv22)
+----------------------------------------------------------------------------------------
+T.FarmMode = false
+SlashCmdList.FARMMODE = function()
+	if T.FarmMode == false then
+		MinimapAnchor:SetSize(C.minimap.size * 1.65, C.minimap.size * 1.65)
+		Minimap:SetSize(MinimapAnchor:GetWidth(), MinimapAnchor:GetWidth())
+		T.FarmMode = true
+	else
+		MinimapAnchor:SetSize(C.minimap.size, C.minimap.size)
+		Minimap:SetSize(MinimapAnchor:GetWidth(), MinimapAnchor:GetWidth())
+		T.FarmMode = false
+	end
+end
+SLASH_FARMMODE1 = "/farmmode"
+SLASH_FARMMODE2 = "/афкььщву"
+SLASH_FARMMODE3 = "/fm"
+SLASH_FARMMODE4 = "/аь"
+
+----------------------------------------------------------------------------------------
+--	Farm mode mouseover button(by m2jest1c)
+----------------------------------------------------------------------------------------
+local farm = CreateFrame("Button", "FarmMode", UIParent)
+farm:SetTemplate("ClassColor")
+if C.actionbar.toggle_mode == true then
+	farm:SetPoint("TOPLEFT", Minimap, "TOPRIGHT", 3, -18)
+else
+	farm:SetPoint("TOPLEFT", Minimap, "TOPRIGHT", 3, 2)
+end
+farm:SetSize(19, 19)
+farm:SetAlpha(0)
+
+farm.t = farm:CreateTexture(nil, "OVERLAY")
+farm.t:SetTexture("Interface\\Icons\\inv_misc_map_01")
+farm.t:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+farm.t:SetPoint("TOPLEFT", farm, 2, -2)
+farm.t:SetPoint("BOTTOMRIGHT", farm, -2, 2)
+
+farm:SetScript("OnClick", function()
+	SlashCmdList.FARMMODE()
+end)
+
+farm:SetScript("OnEnter", function()
+	farm:FadeIn()
+	GameTooltip:SetOwner(farm, "ANCHOR_LEFT")
+	GameTooltip:AddLine(L_MINIMAP_FARM, 0.40, 0.78, 1)
+	GameTooltip:Show()
+end)
+
+farm:SetScript("OnLeave", function()
+	farm:FadeOut()
+	GameTooltip:Hide()
+end)
