009c5085cef3e293ca249c78dbf280bc2f243eb0
Wetxius <wetxius@gmail.com>
2026-03-10 20:24:50 +0300

    [Loot] Added workaround for auto loot to prevent stop looting.

--- ShestakUI/Modules/Loot/FasterLoot.lua
+++ ShestakUI/Modules/Loot/FasterLoot.lua
@@ -1,21 +1,39 @@
 local T, C, L = unpack(ShestakUI)
-if C.loot.faster_loot ~= true then return end
 
 ----------------------------------------------------------------------------------------
 --	Faster auto looting
 ----------------------------------------------------------------------------------------
-local tDelay = 0
-local LOOT_DELAY = 0.3
+if C.loot.faster_loot then
+	local tDelay = 0
+	local LOOT_DELAY = 0.3
 
-local frame = CreateFrame("Frame")
-frame:RegisterEvent("LOOT_READY")
-frame:SetScript("OnEvent", function ()
-	if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
-		if (GetTime() - tDelay) >= LOOT_DELAY then
-			for i = GetNumLootItems(), 1, -1 do
+	local frame = CreateFrame("Frame")
+	frame:RegisterEvent("LOOT_READY")
+	frame:SetScript("OnEvent", function()
+		if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
+			if (GetTime() - tDelay) >= LOOT_DELAY then
+				for i = GetNumLootItems(), 1, -1 do
+					LootSlot(i)
+				end
+				tDelay = GetTime()
+			end
+		end
+	end)
+else
+	local frame = CreateFrame("Frame")
+	frame:RegisterEvent("LOOT_OPENED")
+	frame:SetScript("OnEvent", function()
+		local numItems = GetNumLootItems()
+
+		if numItems == 0 then
+		   CloseLoot()
+		   return
+		end
+
+		if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
+			for i = numItems, 1, -1 do
 				LootSlot(i)
 			end
-			tDelay = GetTime()
 		end
-	end
-end)
+	end)
+end
