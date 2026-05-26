df769828270f292933a90ea4c57a63873fa0e13f
Wetxius <wetxius@gmail.com>
2026-05-26 08:20:45 +0300

    [Tooltip] Added faction check for Harronir.

--- ShestakUI/Modules/Tooltip/Tooltip.lua
+++ ShestakUI/Modules/Tooltip/Tooltip.lua
@@ -336,7 +336,7 @@ local OnTooltipSetUnit = function(self)
 			self:AppendText((" %s"):format("|cffFF0000"..L_CHAT_DND.."|r"))
 		end
 
-		if isPlayer and (englishRace == "Pandaren" or englishRace == "Dracthyr" or englishRace == "EarthenDwarf") and faction ~= nil and faction ~= playerFaction then
+		if isPlayer and (englishRace == "Pandaren" or englishRace == "Dracthyr" or englishRace == "EarthenDwarf" or englishRace == "Harronir") and faction ~= nil and faction ~= playerFaction then
 			local hex = "cffff3333"
 			if faction == "Alliance" then
 				hex = "cff69ccf0"
