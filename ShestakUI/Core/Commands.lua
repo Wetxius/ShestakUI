f00e442fedce4e2a82f851415d44bcf52eef8a53
Wetxius <wetxius@gmail.com>
2026-08-15 16:03:23 +0300

    Fixed /fl error.

--- ShestakUI/Core/Commands.lua
+++ ShestakUI/Core/Commands.lua
@@ -238,7 +238,7 @@ SLASH_FRAME2 = "/акфьу"
 ----------------------------------------------------------------------------------------
 SlashCmdList["FRAMELIST"] = function(msg)
 	if not FrameStackTooltip then
-		UIParentLoadAddOn("Blizzard_DebugTools")
+		C_AddOns.LoadAddOn("Blizzard_DebugTools")
 	end
 
 	local isPreviouslyShown = FrameStackTooltip:IsShown()
@@ -255,7 +255,9 @@ SlashCmdList["FRAMELIST"] = function(msg)
 		local text = _G["FrameStackTooltipTextLeft"..i]:GetText()
 		-- if text and text ~= "" then
 			local r, g, b = _G["FrameStackTooltipTextLeft"..i]:GetTextColor()
-			text = format("|cff%02x%02x%02x%s|r", r * 255, g * 255, b * 255, text)
+			if canaccessvalue(r) then
+				text = format("|cff%02x%02x%02x%s|r", r * 255, g * 255, b * 255, text)
+			end
 			print(text)
 		-- end
 	end
@@ -273,7 +275,6 @@ SLASH_FRAMELIST4 = "/ад"
 --	Frame Stack on Cyrillic
 ----------------------------------------------------------------------------------------
 SlashCmdList.FSTACK = function()
-	-- UIParentLoadAddOn("Blizzard_DebugTools")
 	C_AddOns.LoadAddOn("Blizzard_DebugTools")
 	FrameStackTooltip_Toggle(false, true, true)
 end
