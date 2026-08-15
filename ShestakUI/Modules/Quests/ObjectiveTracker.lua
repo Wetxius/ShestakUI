cb46a0fc339ca0638174f231a5ef43f9eca95da2
Wetxius <wetxius@gmail.com>
2026-08-15 20:21:42 +0300

    [ObjectiveTracker] Disable Auto collapse. SetCollapsed now cause secret taint.

--- ShestakUI/Modules/Quests/ObjectiveTracker.lua
+++ ShestakUI/Modules/Quests/ObjectiveTracker.lua
@@ -90,53 +90,53 @@ end
 --	Auto collapse Objective Tracker
 ----------------------------------------------------------------------------------------
 -- NOTE: SetCollapsed() cause UseQuestLogSpecialItem() taint
-if C.automation.auto_collapse ~= "NONE" then
-	local collapse = CreateFrame("Frame")
-	collapse:RegisterEvent("PLAYER_ENTERING_WORLD")
-	collapse:SetScript("OnEvent", function()
-		if C.automation.auto_collapse == "RAID" then
-			if IsInInstance() then
-				C_Timer.After(0.1, function()
-					ObjectiveTrackerFrame:SetCollapsed(true)
-				end)
-			elseif not InCombatLockdown() then
-				if ObjectiveTrackerFrame.isCollapsed then
-					ObjectiveTrackerFrame:SetCollapsed(false)
-				end
-			end
-		elseif C.automation.auto_collapse == "SCENARIO" then
-			local inInstance, instanceType = IsInInstance()
-			if inInstance then
-				if instanceType == "party" or instanceType == "scenario" then
-					C_Timer.After(0.1, function() -- for some reason it got error after reload in instance
-						for i = 3, #headers do
-							headers[i]:SetCollapsed(true)
-						end
-					end)
-				else
-					C_Timer.After(0.1, function()
-						ObjectiveTrackerFrame:SetCollapsed(true)
-					end)
-				end
-			else
-				if not InCombatLockdown() then
-					for i = 3, #headers do
-						if headers[i].isCollapsed then
-							headers[i]:SetCollapsed(false)
-						end
-					end
-					if ObjectiveTrackerFrame.isCollapsed then
-						ObjectiveTrackerFrame:SetCollapsed(false)
-					end
-				end
-			end
-		elseif C.automation.auto_collapse == "RELOAD" then
-			C_Timer.After(0.1, function()
-				ObjectiveTrackerFrame:SetCollapsed(true)
-			end)
-		end
-	end)
-end
+-- if C.automation.auto_collapse ~= "NONE" then
+	-- local collapse = CreateFrame("Frame")
+	-- collapse:RegisterEvent("PLAYER_ENTERING_WORLD")
+	-- collapse:SetScript("OnEvent", function()
+		-- if C.automation.auto_collapse == "RAID" then
+			-- if IsInInstance() then
+				-- C_Timer.After(0.1, function()
+					-- ObjectiveTrackerFrame:SetCollapsed(true)
+				-- end)
+			-- elseif not InCombatLockdown() then
+				-- if ObjectiveTrackerFrame.isCollapsed then
+					-- ObjectiveTrackerFrame:SetCollapsed(false)
+				-- end
+			-- end
+		-- elseif C.automation.auto_collapse == "SCENARIO" then
+			-- local inInstance, instanceType = IsInInstance()
+			-- if inInstance then
+				-- if instanceType == "party" or instanceType == "scenario" then
+					-- C_Timer.After(0.1, function() -- for some reason it got error after reload in instance
+						-- for i = 3, #headers do
+							-- headers[i]:SetCollapsed(true)
+						-- end
+					-- end)
+				-- else
+					-- C_Timer.After(0.1, function()
+						-- ObjectiveTrackerFrame:SetCollapsed(true)
+					-- end)
+				-- end
+			-- else
+				-- if not InCombatLockdown() then
+					-- for i = 3, #headers do
+						-- if headers[i].isCollapsed then
+							-- headers[i]:SetCollapsed(false)
+						-- end
+					-- end
+					-- if ObjectiveTrackerFrame.isCollapsed then
+						-- ObjectiveTrackerFrame:SetCollapsed(false)
+					-- end
+				-- end
+			-- end
+		-- elseif C.automation.auto_collapse == "RELOAD" then
+			-- C_Timer.After(0.1, function()
+				-- ObjectiveTrackerFrame:SetCollapsed(true)
+			-- end)
+		-- end
+	-- end)
+-- end
 
 ----------------------------------------------------------------------------------------
 --	Difficulty color for quest headers
