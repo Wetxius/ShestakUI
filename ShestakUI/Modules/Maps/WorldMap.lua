b90c911bbe77ddc7c59a2527f242888747e5b3cf
Wetxius <wetxius@gmail.com>
2026-08-19 21:02:07 +0300

    Disable our map coordinate.

--- ShestakUI/Modules/Maps/WorldMap.lua
+++ ShestakUI/Modules/Maps/WorldMap.lua
@@ -35,80 +35,80 @@ numQuest.text:SetText(select(2, C_QuestLog.GetNumQuestLogEntries()).."/"..maxQue
 ----------------------------------------------------------------------------------------
 --	Creating coordinate
 ----------------------------------------------------------------------------------------
-local coords = CreateFrame("Frame", "CoordsFrame", WorldMapFrame)
-coords:SetFrameLevel(WorldMapFrame.BorderFrame:GetFrameLevel() + 2)
-coords:SetFrameStrata(WorldMapFrame.BorderFrame:GetFrameStrata())
+-- local coords = CreateFrame("Frame", "CoordsFrame", WorldMapFrame)
+-- coords:SetFrameLevel(WorldMapFrame.BorderFrame:GetFrameLevel() + 2)
+-- coords:SetFrameStrata(WorldMapFrame.BorderFrame:GetFrameStrata())
 
-coords.PlayerText = coords:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-coords.PlayerText:SetPoint("BOTTOMLEFT", WorldMapFrame.ScrollContainer, "BOTTOM", -40, 20)
-coords.PlayerText:SetJustifyH("LEFT")
-coords.PlayerText:SetText(UnitName("player")..": 0,0")
+-- coords.PlayerText = coords:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+-- coords.PlayerText:SetPoint("BOTTOMLEFT", WorldMapFrame.ScrollContainer, "BOTTOM", -40, 20)
+-- coords.PlayerText:SetJustifyH("LEFT")
+-- coords.PlayerText:SetText(UnitName("player")..": 0,0")
 
-coords.MouseText = coords:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-coords.MouseText:SetJustifyH("LEFT")
-coords.MouseText:SetPoint("BOTTOMLEFT", coords.PlayerText, "TOPLEFT", 0, 5)
-coords.MouseText:SetText(L_MAP_CURSOR..": 0,0")
+-- coords.MouseText = coords:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+-- coords.MouseText:SetJustifyH("LEFT")
+-- coords.MouseText:SetPoint("BOTTOMLEFT", coords.PlayerText, "TOPLEFT", 0, 5)
+-- coords.MouseText:SetText(L_MAP_CURSOR..": 0,0")
 
-local mapRects, tempVec2D = {}, CreateVector2D(0, 0)
-local function GetPlayerMapPos(mapID)
-	tempVec2D.x, tempVec2D.y = UnitPosition("player")
-	if not tempVec2D.x then return end
+-- local mapRects, tempVec2D = {}, CreateVector2D(0, 0)
+-- local function GetPlayerMapPos(mapID)
+	-- tempVec2D.x, tempVec2D.y = UnitPosition("player")
+	-- if not tempVec2D.x then return end
 
-	local mapRect = mapRects[mapID]
-	if not mapRect then
-		local _, pos1 = C_Map.GetWorldPosFromMapPos(mapID, CreateVector2D(0, 0))
-		local _, pos2 = C_Map.GetWorldPosFromMapPos(mapID, CreateVector2D(1, 1))
-		if not pos1 or not pos2 then return end
-		mapRect = {pos1, pos2}
-		mapRect[2]:Subtract(mapRect[1])
-		mapRects[mapID] = mapRect
-	end
-	tempVec2D:Subtract(mapRect[1])
+	-- local mapRect = mapRects[mapID]
+	-- if not mapRect then
+		-- local _, pos1 = C_Map.GetWorldPosFromMapPos(mapID, CreateVector2D(0, 0))
+		-- local _, pos2 = C_Map.GetWorldPosFromMapPos(mapID, CreateVector2D(1, 1))
+		-- if not pos1 or not pos2 then return end
+		-- mapRect = {pos1, pos2}
+		-- mapRect[2]:Subtract(mapRect[1])
+		-- mapRects[mapID] = mapRect
+	-- end
+	-- tempVec2D:Subtract(mapRect[1])
 
-	return (tempVec2D.y/mapRect[2].y), (tempVec2D.x/mapRect[2].x)
-end
+	-- return (tempVec2D.y/mapRect[2].y), (tempVec2D.x/mapRect[2].x)
+-- end
 
-local int = 0
-WorldMapFrame:HookScript("OnUpdate", function()
-	int = int + 1
-	if int >= 3 then
-		local unitMap = C_Map.GetBestMapForUnit("player")
-		local x, y = 0, 0
+-- local int = 0
+-- WorldMapFrame:HookScript("OnUpdate", function()
+	-- int = int + 1
+	-- if int >= 3 then
+		-- local unitMap = C_Map.GetBestMapForUnit("player")
+		-- local x, y = 0, 0
 
-		if unitMap then
-			x, y = GetPlayerMapPos(unitMap)
-		end
+		-- if unitMap then
+			-- x, y = GetPlayerMapPos(unitMap)
+		-- end
 
-		if x and y and x >= 0 and y >= 0 then
-			coords.PlayerText:SetFormattedText("%s: %.0f,%.0f", T.name, x * 100, y * 100)
-		else
-			coords.PlayerText:SetText(UnitName("player")..": ".."|cffff0000"..L_MAP_BOUNDS.."|r")
-		end
+		-- if x and y and x >= 0 and y >= 0 then
+			-- coords.PlayerText:SetFormattedText("%s: %.0f,%.0f", T.name, x * 100, y * 100)
+		-- else
+			-- coords.PlayerText:SetText(UnitName("player")..": ".."|cffff0000"..L_MAP_BOUNDS.."|r")
+		-- end
 
-		if WorldMapFrame.ScrollContainer:IsMouseOver() then
-			local mouseX, mouseY = WorldMapFrame.ScrollContainer:GetNormalizedCursorPosition()
-			if mouseX and mouseY and mouseX >= 0 and mouseY >= 0 then
-				coords.MouseText:SetFormattedText("%s %.0f,%.0f", L_MAP_CURSOR, mouseX * 100, mouseY * 100)
-			else
-				coords.MouseText:SetText(L_MAP_CURSOR.."|cffff0000"..L_MAP_BOUNDS.."|r")
-			end
-		else
-			coords.MouseText:SetText(L_MAP_CURSOR.."|cffff0000"..L_MAP_BOUNDS.."|r")
-		end
+		-- if WorldMapFrame.ScrollContainer:IsMouseOver() then
+			-- local mouseX, mouseY = WorldMapFrame.ScrollContainer:GetNormalizedCursorPosition()
+			-- if mouseX and mouseY and mouseX >= 0 and mouseY >= 0 then
+				-- coords.MouseText:SetFormattedText("%s %.0f,%.0f", L_MAP_CURSOR, mouseX * 100, mouseY * 100)
+			-- else
+				-- coords.MouseText:SetText(L_MAP_CURSOR.."|cffff0000"..L_MAP_BOUNDS.."|r")
+			-- end
+		-- else
+			-- coords.MouseText:SetText(L_MAP_CURSOR.."|cffff0000"..L_MAP_BOUNDS.."|r")
+		-- end
 
-		numQuest.text:SetText(select(2, C_QuestLog.GetNumQuestLogEntries()).."/"..maxQuest)
+		-- numQuest.text:SetText(select(2, C_QuestLog.GetNumQuestLogEntries()).."/"..maxQuest)
 
-		int = 0
-	end
-end)
+		-- int = 0
+	-- end
+-- end)
 
-coords:RegisterEvent("PLAYER_ENTERING_WORLD")
-coords:SetScript("OnEvent", function(self, event)
-	self:UnregisterEvent(event)
-	if ShestakUISettingsPerChar and ShestakUISettingsPerChar.Coords ~= true then
-		coords:SetAlpha(0)
-	end
-end)
+-- coords:RegisterEvent("PLAYER_ENTERING_WORLD")
+-- coords:SetScript("OnEvent", function(self, event)
+	-- self:UnregisterEvent(event)
+	-- if ShestakUISettingsPerChar and ShestakUISettingsPerChar.Coords ~= true then
+		-- coords:SetAlpha(0)
+	-- end
+-- end)
 
 ----------------------------------------------------------------------------------------
 --	Added options to map tracking button
@@ -151,21 +151,21 @@ function MapFrame.Menu(self, level)
 	info.isNotRadio = true
 	info.keepShownOnClick = true
 
-	info.text = L_MAP_COORDS
-	info.checked = function()
-		return ShestakUISettingsPerChar.Coords == true
-	end
+	-- info.text = L_MAP_COORDS
+	-- info.checked = function()
+		-- return ShestakUISettingsPerChar.Coords == true
+	-- end
 
-	info.func = function()
-		if ShestakUISettingsPerChar.Coords == true then
-			ShestakUISettingsPerChar.Coords = false
-			coords:SetAlpha(0)
-		else
-			ShestakUISettingsPerChar.Coords = true
-			coords:SetAlpha(1)
-		end
-	end
-	UIDropDownMenu_AddButton(info)
+	-- info.func = function()
+		-- if ShestakUISettingsPerChar.Coords == true then
+			-- ShestakUISettingsPerChar.Coords = false
+			-- coords:SetAlpha(0)
+		-- else
+			-- ShestakUISettingsPerChar.Coords = true
+			-- coords:SetAlpha(1)
+		-- end
+	-- end
+	-- UIDropDownMenu_AddButton(info)
 
 	if C.minimap.fog_of_war == true then
 		info.text = L_MAP_FOG
