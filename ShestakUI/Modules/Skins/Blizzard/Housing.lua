77afe96142c65c8463f9d14d319fd1443a0b608e
Wetxius <wetxius@gmail.com>
2026-08-30 17:23:58 +0300

    [Skins] More Housing skin.

--- ShestakUI/Modules/Skins/Blizzard/Housing.lua
+++ ShestakUI/Modules/Skins/Blizzard/Housing.lua
@@ -31,7 +31,7 @@ local function LoadSkin()
 		end
 
 		if tab.SelectedTexture then
-			tab.SelectedTexture:SetDrawLayer('ARTWORK')
+			tab.SelectedTexture:SetDrawLayer("ARTWORK")
 			tab.SelectedTexture:SetColorTexture(1, 0.82, 0, 0.3)
 			tab.SelectedTexture:SetInside(tab.backdrop)
 		end
@@ -57,7 +57,129 @@ local function LoadSkin()
 		end
 	end
 
+	T.SkinDropDownBox(frame.HouseDropdown.Dropdown)
+
 	frame.HouseInfoTabButton:SetPoint("TOPLEFT", frame, "TOPRIGHT", 1, -60)
+
+	local infoContent = frame.HouseInfoContent
+	if infoContent then
+		infoContent.DashboardNoHousesFrame.NoHouseButton:SkinButton()
+		infoContent.HouseFinderButton:SkinButton()
+
+		local contentFrame = infoContent.ContentFrame
+		if contentFrame then
+			local HouseUpgradeFrame = contentFrame.HouseUpgradeFrame
+			if HouseUpgradeFrame then
+				HouseUpgradeFrame:StripTextures()
+				HouseUpgradeFrame.Background:Hide()
+				HouseUpgradeFrame.TrackFrame.Background:Hide()
+				T.SkinCheckBox(HouseUpgradeFrame.WatchFavorButton)
+			end
+
+			hooksecurefunc(contentFrame, "UpdateTabs", function(frame)
+				for _, tab in next, {frame.TabSystem:GetChildren()} do
+					T.SkinTab(tab, true)
+					tab.backdrop:SetPoint("TOPLEFT", 0, -3)
+					tab.backdrop:SetPoint("BOTTOMRIGHT", 0, 3)
+				end
+			end)
+
+			hooksecurefunc(HouseUpgradeFrame, "SetRewards", function(self)
+				-- TODO
+				-- for reward in self.rewardPool:EnumerateActive() do
+					-- if not reward.styled then
+						-- reward:CreateBackdrop("Overlay")
+						-- reward.backdrop:SetPoint("TOPLEFT", reward, 18, -8)
+						-- reward.backdrop:SetPoint("BOTTOMRIGHT", reward, -18, 8)
+
+						-- reward.RewardCardBG:SetAlpha(0)
+
+						-- reward.b = CreateFrame("Frame", nil, reward)
+						-- reward.b:SetTemplate("Default")
+						-- reward.b:SetPoint("TOPLEFT", reward.RewardCardIcon, "TOPLEFT", -2, 2)
+						-- reward.b:SetPoint("BOTTOMRIGHT", reward.RewardCardIcon, "BOTTOMRIGHT", 2, -2)
+						-- reward.RewardCardIcon:SetParent(reward.b)
+						-- reward.RewardCardIcon:SetTexCoord(0.15, 0.85, 0.15, 0.85)
+						-- reward.styled = true
+					-- end
+				-- end
+			end)
+		end
+
+		local initiativesFrame = contentFrame.InitiativesFrame
+		if initiativesFrame then
+			initiativesFrame.InitiativesArt:Hide()
+
+			local bar = initiativesFrame.InitiativeSetFrame.ProgressBar
+			bar.barBackdrop = CreateFrame("Frame", nil, bar)
+			bar.barBackdrop:SetFrameLevel(bar:GetFrameLevel())
+			bar.barBackdrop:SetOutside(bar)
+			bar.barBackdrop:SetTemplate("Overlay")
+			bar:SetStatusBarTexture(C.media.texture)
+			bar:SetStatusBarColor(0, 0.6, 1)
+
+			-- TODO
+			-- hooksecurefunc(MonthlyActivitiesRewardButtonMixin, "SetRewardItem", function(self)
+				-- if not self.styled then
+					-- self:ClearAllPoints()
+					-- self:SetPoint("LEFT", monthlyActivities.ThresholdContainer.ThresholdBar.barBackdrop, "RIGHT", -2, -3)
+					-- self.Icon:SkinIcon()
+					-- self.CircleMask:Hide()
+					-- self.NormalTexture:SetAlpha(0)
+					-- self.HighlightTexture:SetAlpha(0)
+					-- self.PushedTexture:SetAlpha(0)
+					-- self.styled = true
+				-- end
+			-- end)
+
+			local tasks = initiativesFrame.InitiativeSetFrame.InitiativeTasks
+			if tasks then
+				tasks:CreateBackdrop("Overlay")
+				tasks.backdrop:SetInside()
+				tasks.backdrop:SetPoint("TOPLEFT", 2, 5)
+				tasks.backdrop:SetPoint("BOTTOMRIGHT", -1, 2)
+				tasks.TaskListTitleContainer:StripTextures()
+				T.SkinScrollBar(tasks.ScrollBar)
+
+				for _, frame in next, {
+					tasks.BG,
+					tasks.BorderRight,
+					tasks.BorderTop,
+					tasks.TitleCornerBR,
+					tasks.TitleCornerTR,
+					tasks.TaskListTitleContainer.TitleCornerBR,
+					tasks.TaskListTitleContainer.TitleFoliage
+				} do
+					if frame then
+						frame:Hide()
+					end
+				end
+			end
+
+			local activity = initiativesFrame.InitiativeSetFrame.InitiativeActivity
+			if activity then
+				activity:CreateBackdrop("Overlay")
+				activity.backdrop:SetPoint("TOPLEFT", 3, 5)
+				activity.backdrop:SetPoint("BOTTOMRIGHT", -1, 2)
+				activity.ActivityLogTitleContainer:StripTextures()
+				T.SkinScrollBar(activity.ScrollBar)
+
+				for _, frame in next, {
+					activity.BG,
+					activity.BGTexture,
+					activity.BorderTop,
+					activity.TitleCornerBL,
+					activity.TitleCornerTR,
+					activity.ActivityLogTitleContainer.TitleCornerBL,
+					activity.ActivityLogTitleContainer.TitleFoliage
+				} do
+					if frame then
+						frame:Hide()
+					end
+				end
+			end
+		end
+	end
 
 	local content = frame.CatalogContent
 	content:StripTextures()
@@ -212,6 +334,12 @@ local function LoadHouseSettings()
 	frame.AbandonHouseButton:SkinButton()
 	frame.IgnoreListButton:SkinButton()
 	frame.SaveButton:SkinButton()
+
+
+	frame = AbandonHouseConfirmationDialog
+	T.SkinFrame(frame)
+	frame.ConfirmButton:SkinButton()
+	frame.CancelButton:SkinButton()
 end
 
 T.SkinFuncs["Blizzard_HousingHouseSettings"] = LoadHouseSettings
