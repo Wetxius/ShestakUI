5ef2bad7e1681f14d0c04e79e2625cc9610e5070
Wetxius <wetxius@gmail.com>
2026-08-30 11:04:22 +0300

    [RaidAuraWatch] Updated Earth Shield spellid.

--- ShestakUI/Config/Filters/RaidAuraWatch.lua
+++ ShestakUI/Config/Filters/RaidAuraWatch.lua
@@ -63,7 +63,7 @@ if C.raidframe.plugins_buffs then
 		},
 		SHAMAN = {
 			{61295, "TOPRIGHT", {0.7, 0.3, 0.7}},				-- Riptide
-			{974, "BOTTOMRIGHT", {0.91, 0.80, 0.44}},			-- Earth Shield
+			{383648, "BOTTOMRIGHT", {0.91, 0.80, 0.44}},		-- Earth Shield
 			{382024, "BOTTOMLEFT", {0.8, 0.9, 0.7}},			-- Earthliving Weapon
 		},
 		HUNTER = {
